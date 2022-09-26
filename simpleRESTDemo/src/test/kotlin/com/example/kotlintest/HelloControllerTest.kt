6e547022e9eb9b3dc802f5fdd095d31e8c74c6d2
zach lewis <zlewis@clarityinnovates.com>
2022-09-26 13:07:10 -0500

    test added

---
+++ simpleRESTDemo/src/test/kotlin/com/example/kotlintest/HelloControllerTest.kt
@@ -0,0 +1,22 @@
+package com.example.kotlintest;
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@WebMvcTest
+class HelloControllerTest(@Autowired val mockMvc: MockMvc) {
+
+
+ @Test
+    fun helloWorks() {
+
+        mockMvc.perform(get("/?name=zac"))
+        .andExpect(status().isOk)
+    }
+
+    }
